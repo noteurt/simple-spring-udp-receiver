4f802bff60255bf3154172097a90e89f0649f431
noteurt <rausch.pascal@gmx.de>
2022-11-15 17:16:54 +0100

    add function to render bytes to command prompt

--- src/main/java/com/example/updReceiveServerIntegration/service/UdpService.java
+++ src/main/java/com/example/updReceiveServerIntegration/service/UdpService.java
@@ -2,11 +2,13 @@ package com.example.updReceiveServerIntegration.service;
 
 import org.springframework.messaging.Message;
 
+import java.util.Arrays;
+
 public class UdpService {
 
-    public void receive(Message message) {
-        String data = new String((byte[]) message.getPayload());
-        System.out.print(data);
+    public void receive(byte[] message) {
+        //String data = new String((byte[]) message.getPayload());
+        System.out.println(Arrays.toString(message));
     }
 }
 
